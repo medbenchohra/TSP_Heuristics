b4bcfb0bb2bd5a208cfd83ff4df7bfdcfef6849f
BENMALEKarim <fk_benmalek@esi.dz>
2019-07-13 23:10:40 +0100

    corrected two-opt && GA

--- GUI/scripts/two_opt_tsp/two_opt.py
+++ GUI/scripts/two_opt_tsp/two_opt.py
@@ -11,13 +11,14 @@ params=json.loads(sys.argv[1])
 
 coords=[]
 instance=TsplibParser.load_instance("scripts/tsp-dataset/"+params["fileName"],None)
-adj_matrix = instance.get_adj_matrix()
-init_route = list(range(adj_matrix.shape[0]))
+
+adj_matrix = instance.get_adj_matrix().tolist()
+init_route = list(range(len((adj_matrix))))
 init_route.append(0)
+
 
 def cost_change(cost_mat, n1, n2, n3, n4):
     return cost_mat[n1][n3] + cost_mat[n2][n4] - cost_mat[n1][n2] - cost_mat[n3][n4]
-
 
 def heuristic_two_opt(route, cost_mat):
     best = route
@@ -32,10 +33,11 @@ def heuristic_two_opt(route, cost_mat):
                     best[i:j] = best[j - 1:i - 1:-1]
                     improved = True
         route = best
-    for _ in range(0, len(best) - 1):
-        cost += cost_mat[best[_]][best[_ + 1]]
-
+    for _ in range(0,len(best)-1):
+        cost += cost_mat[best[_]][best[_+1]]
+    
     return cost
+
 
 if __name__ == "__main__":
     t1=time.time()
